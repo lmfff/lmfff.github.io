b0962e1b6542a99bbe912a98587c8c27524c1acd
lmfff <lorenzomf95@gmail.com>
2017-02-19 19:54:21 +0100

    Project 6

--- StudyingP5js/Project6/sketch.js
+++ StudyingP5js/Project6/sketch.js
@@ -1,26 +1,57 @@
-bubbles=[];
+var bubbles=[];
+var click = false;
 
-function bubble(){
-    this.x=mouseX;
-    this.y=mouseY;
-    this.r = random(5, 50);
-    this.display = function() {
-        noStroke();
-        fill(200);
-        ellipse(this.x, this.y, this.r, this.r);
-    }
-    this.change = function() {
-        this.r = this.r + random(-5, 5);
-    }
+function setup(){
+    createCanvas(600, 600);
+    background(0);
+    
 }
-function setup() {
-    createCanvas(500, 500);
-    for (var i = 0; i < 10; i++) {
-        bubbles[i] = new bubble();
+
+function mousePressed(){
+    click = true;
+    for (var i = 0; i < 2500; i++){
+        bubbles[i]={
+            x: mouseX,
+            y: mouseY,
+            display: function(){
+                noStroke();
+                fill(random(0,200),random(0,200),random(0,200),25);
+                ellipse(this.x, this.y, 10, 10);
+            },
+            move: function(){
+                this.x = this.x + random(-4, 4);
+                this.y = this.y + random(-4, 4);
+            }
+        }
     }
 }
 
 function draw(){
-
-    
-}
+    background(0);
+    if (click == false) {
+        fill(20, 20, 20);
+    }
+    rect(100, 100, 400, 400);
+    for (var i = 0; i < bubbles.length; i++) {
+        
+        bubbles[i].display();
+        
+        bubbles[i].move();
+        
+        if (bubbles[i].x > 500) {
+            bubbles[i].x = 500;
+        }
+        
+        if (bubbles[i].x < 100) {
+            bubbles[i].x = 100;
+        }
+        
+        if (bubbles[i].y > 500) {
+            bubbles[i].y = 500;
+        }
+        
+        if (bubbles[i].y < 100) {
+            bubbles[i].y = 100;
+        }
+    }
+}    
