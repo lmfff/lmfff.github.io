0282b3e7276947b6bae5553998b119b3c7e5f2c6
lmf <lorenzomf95@gmail.com>
2017-07-05 01:51:04 +0200

    root + a

--- thecomforters03/scores.js
+++ thecomforters03/scores.js
@@ -1,10 +1,13 @@
 var debugMode = false
 
 var score = {
+    pos: 'root',
     txt: ['Caroline began to pack her things.', 'She heard loud noises coming from outside her room,', 'again,', 'choice', 'She stops to listen.', 'She leaves'],
     a: { 
-         txt: ['loud enough to make her stop and listen;', 'Caroline found her neighbours\' arguments entertaining.', 'There were days when they could go on for hours and HOURS', 'about the smallest things.', 'They could never seem to just let go.', 'And Caroline could never seem to stop listening', 'to their lengthy arguments.', /* newscene */ 'She ran to find a cab,', 'water splashing on her shoes and dripping down her coat.', 'Caroline has lost far too much time inside.', 'Now it was pouring and she lost her train.', 'There were many miles ahead.', 'Caroline tried to lay her head down', 'even though the driver\'s glances through the mirror made her feel uncofortable.' ],
+        pos: 'a',
+         txt: ['loud enough to make her stop and listen.', 'Caroline found her neighbours\' arguments entertaining.', 'There were days when they could go on for hours,', 'about the most insignificant things.', 'They could never seem to just let go.', /* newscene */ 'Caroline ran to find a cab,', 'water splashing on her shoes and dripping down her coat.', 'She had lost far too much time inside.', 'Now it was pouring and she lost her train.', 'There were many miles ahead.', 'Caroline tried to lay her head down', 'even though the driver\'s glances through the mirror made her feel uncofortable.' ],
         a: { 
+            pos: 'aa',
              txt: ['Fino ad ora hai scelto She stops to listen e a'],
             a: { 
                 
@@ -14,6 +17,7 @@ var score = {
             }
         },
         b: { 
+            pos: 'ab',
              txt: ['Fino ad ora hai scelto She stops to listen e b'],
             a: { 
 
@@ -23,9 +27,11 @@ var score = {
             }
         }
     },
-    b: { 
+    b: {
+        pos: 'b',
          txt: ['but time was running, so Caroline grabbed her notes', 'and left.', /* new scene */ 'Her train was right on time.', 'As soon as she took her seat she started drifting:', 'Caroline looked forward to that moment everyday,', 'when she could abandon herself to the sensantions,', 'of her memories,', 'moved uncontrollably,', 'yet so delicate'],
         a: { 
+            pos: 'ba',
              txt: ['Fino ad ora hai scelto She leaves e c'],
             a: { 
 
@@ -35,6 +41,7 @@ var score = {
             }
         },
         b: { 
+            pos: 'bb',
              txt: ['Fino ad ora hai scelto She leaves e d'],
             a: { 
 
@@ -64,7 +71,7 @@ function loadRestScore(){
     restScore = {
         rest: [luggage.duration()*1000 - 2000, 1000, 100],
         a: {
-            rest: [/*choice*/100, 1000, 700, 500, 700, 1000, 100, /*newscene*/ 5000, 1000, 1000, 500, 100 ],
+            rest: [/*choice*/100, 1000, 700, 500, 700, 2000, /*newscene*/ 5000, 1000, 1000, 500, 100 ],
             a: {},
             b: {}
         },
@@ -82,11 +89,13 @@ function loadRestScore(){
 
 function soundScore() {
     var q = globalCharCount
-    console.log(q)
-    switch (true) {
+    //console.log(q)
+    if (pointer.pos === 'root') {
+        switch (true) {
         case (q === 0):
             var roomVol = ( (globalCharCount + 1) / title.length )
             roomTone1.loop(0, 1, roomVol, 10, 60)
+            
             break;
         case (q < title.length - 1):
             var roomVol = ( (globalCharCount + 1) / title.length ) * 2.5
@@ -101,11 +110,33 @@ function soundScore() {
             foley1.play(0, 1, 0.3)
             break;
         case (q === 48):
-            foley1.setVolume(0, 1)
-            foley1.stop(1)
+            foley1.setVolume(0, 3)
+            foley1.pause(3)
             luggage.play(0, 1, 0.5)
+            setTimeout(function(){
+                foley1.play(0, 1, 0.3)
+            }, luggage.duration() * 1000)
+            break;
+        case (q === 49):
+            console.log(pointer.txt[scoreCount])
+            var filter = new p5.LowPass()
+            filter.freq(500)
+            var argumentVol = 0
+            argument.pan(0.7)
+            argument.disconnect()
+            argument.connect(filter)
+            argument.play(0, 1, argumentVol)
+            console.log(argument.duration())
+            break;
+        case (q > 49 && q < 49 + pointer.txt[scoreCount].length):
+            argumentVol = ((globalCharCount - 49) / pointer.txt[scoreCount].length)
+            argumentVol = Math.min(Math.max(argumentVol, 0), 0.7)
+            console.log(argumentVol)
+            argument.setVolume( argumentVol, 0.10)
             break;
                            }
+    }
+    
     
     
 }
--- thecomforters03/sketch.js
+++ thecomforters03/sketch.js
@@ -1,7 +1,7 @@
 var pointer = score
 var restPointer = restScore
 
-var choiceScore = ['She stops to listen.', 'She leaves.', /*'Test Left'*/, /*'Test Right'*/]
+var choiceScore = ['She stops to listen.', 'She leaves.', /*'Test Left'*/ , /*'Test Right'*/ ]
 
 var choiceFillR = 100
 var choiceFillL = 100
@@ -32,6 +32,9 @@ var title = 'THE COMFORTERS'
 var titleFill = 0
 var exKeyCode
 var exExKeyCode
+var typeGate = false
+var typeMaxV = 50
+var fired = false
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
@@ -46,13 +49,14 @@ function setup() {
 }
 
 function preload() {
-    typeSingle = loadSound( '/src/typeSingle.mp3')
-    typeSpace = loadSound( '/src/typeSpace.mp3')
-    typeBell = loadSound( '/src/typeBell.mp3')
-    roomTone1 = loadSound( '/src/roomTone1.mp3')
-    luggage = loadSound( '/src/luggage.wav')
-    foley1 = loadSound( '/src/foley1.wav')
-    
+    typeSingle = loadSound('/src/typeSingle.mp3')
+    typeSpace = loadSound('/src/typeSpace.mp3')
+    typeBell = loadSound('/src/typeBell.mp3')
+    roomTone1 = loadSound('/src/roomTone1.mp3')
+    luggage = loadSound('/src/luggage.wav')
+    foley1 = loadSound('/src/foley1.wav')
+    argument = loadSound('/src/argument.wav')
+
 }
 
 function draw() {
@@ -72,14 +76,14 @@ function draw() {
     textSize(50)
     if (titleState) {
         inputAllowed = true
-        text(titleRend + stopBar, width/2, height - 80 - (25 * (hCount + 1)))
+        text(titleRend + stopBar, width / 2, height - 80 - (25 * (hCount + 1)))
     } else {
-        text(title, width/2, height - 80 - (25 * (hCount + 1)))
+        text(title, width / 2, height - 80 - (25 * (hCount + 1)))
         textSize(20)
-        text(textRend + stopBar, width/2, height - 50 - (25 * hCount))
+        text(textRend + stopBar, width / 2, height - 50 - (25 * hCount))
         choiceRender()
     }
-    
+
 }
 
 
@@ -93,11 +97,19 @@ function keyPressed() {
     if (pointer.txt[scoreCount] === 'choice') {
         inputAllowed = false
     }
-    if (titleState && keyCode != exKeyCode && keyCode != exExKeyCode) {
+    if (titleState && keyCode != exKeyCode && keyCode != exExKeyCode && !typeGate) {
+        typeGate = true
+        setTimeout(function () {
+            typeGate = false
+        }, typeMaxV)
         globalCharCount++
         soundScore()
-        typeSingle.play()
         charCount++
+        if (title.charAt(charCount) === ' ') {
+            typeSpace.play()
+        } else {
+            typeSingle.play()
+        }
         exExKeyCode = exKeyCode
         exKeyCode = keyCode
         titleRend += title.charAt(charCount)
@@ -106,14 +118,18 @@ function keyPressed() {
             charCount = -1
             inputAllowed = false
             stopBarIsLoading = true
-            setTimeout(function() { 
+            setTimeout(function () {
                 inputAllowed = true
                 stopBarIsLoading = false
-                
+
             }, 1500)
         }
     }
-    if (inputAllowed && keyCode != exKeyCode && keyCode != exExKeyCode && !titleState) {
+    if (inputAllowed && keyCode != exKeyCode && keyCode != exExKeyCode && !titleState && !typeGate) {
+        typeGate = true
+        setTimeout(function () {
+            typeGate = false
+        }, typeMaxV)
         globalCharCount++
         soundScore()
         charCount++
@@ -125,12 +141,12 @@ function keyPressed() {
             charCount = -1
             inputAllowed = false
             stopBarIsLoading = true
-            setTimeout(function() { 
+            setTimeout(function () {
                 inputAllowed = true
                 stopBarIsLoading = false
             }, restPointer.rest[restCount])
             if (pointer.txt[scoreCount] === 'choice') {
-                inputAllowed = false                
+                inputAllowed = false
             }
             scoreCount++
             hCount++
@@ -138,6 +154,33 @@ function keyPressed() {
         }
     }
     if (pointer.txt[scoreCount] === 'choice') {
+        if (pointer.pos === 'root' && !fired) {
+            fired = true
+            setTimeout(function () {
+                choiceMade = 'a'
+                choiceFillL = 250
+                choiceFillR = 100
+                typeSingle.play()
+            }, 5000)
+            setTimeout(function () {
+                typeSpace.play()
+                pointer = pointer.a
+                restPointer = restPointer.a
+                scoreCount = 0
+                restOff = 0
+                restCount = 0
+                choiceMade = undefined
+                choiceFillL = 100
+                choiceFillR = 100
+                restOff += 1
+                restCount = scoreCount + restOff
+                setTimeout(function () {
+                    inputAllowed = true
+                    stopBarIsLoading = false
+                }, restPointer.rest[restCount])
+            }, 6000)
+        }
+
         if (keyCode === RIGHT_ARROW) {
             choiceMade = 'b'
             choiceFillR = 250
@@ -155,20 +198,20 @@ function keyPressed() {
                 scoreCount = 0
                 restOff = 0
                 restCount = 0
-                setTimeout(function() { 
-                inputAllowed = true
-                stopBarIsLoading = false
-            }, restPointer.rest[restCount])
+                setTimeout(function () {
+                    inputAllowed = true
+                    stopBarIsLoading = false
+                }, restPointer.rest[restCount])
             } else {
                 pointer = pointer.b
                 restPointer = restPointer.b
                 scoreCount = 0
                 restOff = 0
                 restCount = 0
-                setTimeout(function() { 
-                inputAllowed = true
-                stopBarIsLoading = false
-            }, restPointer.rest[restCount])
+                setTimeout(function () {
+                    inputAllowed = true
+                    stopBarIsLoading = false
+                }, restPointer.rest[restCount])
             }
             choiceMade = undefined
             choiceFillL = 100
@@ -184,10 +227,10 @@ function choiceRender() {
         if (pointer.txt[scoreCount] === 'choice') {
             textAlign(RIGHT)
             fill(choiceFillL)
-            text(choice1, width/2 - 10, height - 50)
+            text(choice1, width / 2 - 10, height - 50)
             textAlign(LEFT)
             fill(choiceFillR)
-            text(choice2, width/2 + 10, height - 50)
+            text(choice2, width / 2 + 10, height - 50)
         }
     }
 }
@@ -210,12 +253,12 @@ function stopBarFlash() {
 
 function stopBarLoad() {
     if (frameCount % 20 === 5) {
-       stopBar = "\\"
+        stopBar = "\\"
     } else if (frameCount % 20 === 10) {
-       stopBar = '|' 
+        stopBar = '|'
     } else if (frameCount % 20 === 15) {
-       stopBar = '/' 
-    }else if (frameCount % 20 === 19) {
-       stopBar = '-' 
+        stopBar = '/'
+    } else if (frameCount % 20 === 19) {
+        stopBar = '-'
     }
 }
